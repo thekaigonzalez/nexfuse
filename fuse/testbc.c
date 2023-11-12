1b4f2e2fe52301c1482dfe881893982d752e0586
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-11 23:10:10 -0500

    add new test

---
+++ fuse/testbc.c
@@ -0,0 +1,38 @@
+/*Copyright 2019-2023 Kai D. Gonzalez*/
+
+#include "fbcc.h"
+#include "fvcpu.h"
+#include "fmem.h"
+#include "flist.h"
+#include "fctx.h"
+#include "fdef.h"
+#include "ffnmap.h"
+#include "fbcp.h"
+
+main (void)
+{
+  F_Cpu c;
+  FBytecodeChunk *chunk = FBytecodeChunkInit ();
+
+  FBytecodeChunkMany (chunk, 
+  INITSEC, 1, NNULL,
+  INIT, 1, NNULL,
+
+  SUB, 8,
+  ECHO, 0x47, NNULL,
+  END,
+  ENDSUB,
+
+  GOSUB, 8, NNULL,
+  GOSUB, 8, NNULL,
+  
+  END,
+  __END__);
+
+  // printf("RR %d\n", c.reg[1].data[7]);
+
+  byte result = CPRunBytecode (&c, chunk);
+
+
+  FBytecodeChunkFree (chunk);
+}

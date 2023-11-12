95e795579ab4d3b77bf191cd015f4ed404946ba9
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-12 09:44:51 -0500

    add better error handling

--- fuse/fbcc.c
+++ fuse/fbcc.c
@@ -1,11 +1,19 @@
 #include "fbcc.h"
+#include "fmem.h"
+#include "fdef.h"
 
 FBytecodeChunk *
 FBytecodeChunkInit ()
 {
-  FBytecodeChunk * chunk = (FBytecodeChunk *) malloc (sizeof (FBytecodeChunk));
-  
-  chunk->ptr = (byte *) malloc (FUSE_INITIAL_SIZE);
+  FBytecodeChunk *chunk = (FBytecodeChunk *)malloc (sizeof (FBytecodeChunk));
+
+  if (chunk == NULL)
+    {
+      FFree (chunk);
+      return NULL;
+    }
+
+  chunk->ptr = (byte *)malloc (FUSE_INITIAL_SIZE);
   chunk->size = 0;
 
   return chunk;
@@ -14,9 +22,15 @@ FBytecodeChunkInit ()
 void
 FBytecodeChunkAppend (FBytecodeChunk *chunk, byte ptr)
 {
+  if (chunk == NULL)
+    {
+      FError ("fuse: invalid bytecode chunk\n");
+      FBytecodeChunkFree (chunk);
+      return;
+    }
   chunk->ptr = realloc (chunk->ptr, chunk->size + 1);
-  chunk->ptr [chunk->size] = ptr;
-  chunk->size ++;
+  chunk->ptr[chunk->size] = ptr;
+  chunk->size++;
 }
 
 void
@@ -27,25 +41,31 @@ FBytecodeChunkMany (FBytecodeChunk *chunk, ...)
   va_start (args, chunk);
 
   while (1)
-   {
-     byte ptr = va_arg (args, int);
+    {
+      byte ptr = va_arg (args, int);
 
-     if (ptr == (byte) __END__) break;
+      if (ptr == (byte)__END__)
+        break;
 
-     FBytecodeChunkAppend (chunk, ptr);
-   }
+      FBytecodeChunkAppend (chunk, ptr);
+    }
 }
 
 void
 FBytecodeChunkReset (FBytecodeChunk *chunk)
 {
   chunk->size = 0;
-  chunk->ptr = (byte *) realloc (chunk->ptr, FUSE_INITIAL_SIZE);
+  chunk->ptr = (byte *)realloc (chunk->ptr, FUSE_INITIAL_SIZE);
 }
 
 void
 FBytecodeChunkFree (FBytecodeChunk *chunk)
 {
+  if (chunk == NULL)
+    return;
+  if (chunk->ptr == NULL)
+    return;
+
   free (chunk->ptr);
   free (chunk);
 }

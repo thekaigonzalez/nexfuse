d749c404ecc83e71f1faab473b76714b39ebb324
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-12 12:33:45 -0500

    implement simple outputs

---
+++ fuse/fou.h
@@ -0,0 +1,16 @@
+/*Copyright 2019-2023 Kai D. Gonzalez*/
+
+#ifndef FUSE_FOU_H
+#define FUSE_FOU_H
+
+// FUSE utilities (output, etc)
+
+#include "fdef.h"
+#include "fbcc.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void FPrintBytecodeChunk (FBytecodeChunk *chunk);
+
+#endif
